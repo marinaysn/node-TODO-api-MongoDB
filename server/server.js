c8f31de97af4ee89f541760c0a3b20e0c890601e
marinaysn <marinaysn@gmail.com>
2018-12-18 00:04:40 -0500

    adding User Model

--- server/server.js
+++ server/server.js
@@ -6,37 +6,74 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
-let Todo = mongoose.model("Todo", {
-  text: {
+// let Todo = mongoose.model("Todo", {
+//   text: {
+//     type: String,
+//     required: [true, "Must enter todo text"],
+//     unique: true,
+//     minlength: 2,
+//     maxlength: 25,
+//     trim: true //removes whitespaces
+//   },
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   },
+//   completedAt: {
+//       type: Number,
+//       default: null
+//   }
+// });
+
+// let newTodo = new Todo({
+//   text: "Send Letter to Dad",
+//   completedAt: 1955
+// });
+
+// newTodo.save().then(
+//   doc => {
+//     console.log("Save todo", doc);
+//   },
+//   e => {
+//     console.log("Unable to save todo");
+//   }
+// );
+
+let User = mongoose.model("User", {
+  name: {
     type: String,
-    required: [true, "Must enter todo text"],
-    unique: true,
-    minlength: 2,
-    maxlength: 25,
-    trim: true //removes whitespaces
+    require: true
   },
-  completed: {
-    type: Boolean,
-    default: false
+  email: {
+    type: String,
+    minlength: 6,
+    require: true,
+    trim: true
   },
-  completedAt: {
-      type: Number,
-      default: null
+  location: {
+    type: String,
+    default: "New York"
+  },
+  age: {
+    type: Number,
+    default: 15
+  },
+  gender: {
+    type: String
   }
 });
 
-let newTodo = new Todo({
-  text: "Send Letter to Dad",
-  completedAt: 1955
+let newUser = new User({
+    name: 'Paul',
+    age: 42,
+    email: 'paul@ny.com',
+    gender: 'male'
 });
 
-newTodo.save().then(
-  doc => {
-    console.log("Save todo", doc);
-  },
-  e => {
-    console.log("Unable to save todo");
-  }
-);
+newUser.save().then(doc => {
+    console.log(`Saved user`);
+}, (e) => {
+    console.log(`Unable to save user`);
+})
 
 //mongoose.close();
