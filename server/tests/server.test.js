b883973f3ee51ca085f1a36241ca8f3c0593d5dd
marinaysn <marinaysn@gmail.com>
2018-12-21 00:27:54 -0500

    adding extra tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,48 +6,68 @@ const { app } = require("./../server.js");
 const { Todo } = require("./../models/todo.js");
 let i;
 
-beforeEach((done) => {
-    //Todo.remove({}).then(() => done());
-    Todo.count().then((count) => {
-        i = count
-        console.log(` count is : ${i}`)
+beforeEach(done => {
+  //Todo.remove({}).then(() => done());
+  Todo.count()
+    .then(count => {
+      i = count;
+      console.log(` count is : ${i}`);
     })
-        .then(() => done());
-
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
-
-    it("should create a new todo", (done) => {
-
-    let text = "Test todo text mm1";
+  it("should create a new todo", done => {
+    let text = "Wash everything";
 
     request(app)
-      .post('/todos')
-      .send({text})
+      .post("/todos")
+      .send({ text })
       .expect(200)
-      .expect((res) => {
+      .expect(res => {
         expect(res.body.text).toBe(text);
       })
       .end((err, res) => {
         if (err) {
           return done(err);
         }
-        Todo.find().then((todos) => {
-            expect(todos.length).toBe(i+1);
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(i + 1);
             expect(todos[i].text).toBe(text);
             done();
           })
-          .catch((e) => done(e));
+          .catch(e => done(e));
+      });
+  });
+
+  it("Should not create todo with invalid data", (done) => {
+    request(app)
+      .post("/todos")
+      .send({})
+      .expect(400)
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find()
+          .then(todos => {
+              expect(todos.length).toBe(i);
+              console.log(` count is : ${i}`);
+              console.log(` tl is : ${todos.length}`);
+            done();
+          })
+          .catch(e => done(e));
       });
   });
 });
 
-    // Todo.find().count().then(
-    //     count => {
-    //       i = count
-    //     },
-    //     err => {
-    //       console, log("Unable to fetch the data from Todos", err);
-    //     }
-    //   );
+// Todo.find().count().then(
+//     count => {
+//       i = count
+//     },
+//     err => {
+//       console, log("Unable to fetch the data from Todos", err);
+//     }
+//   );
