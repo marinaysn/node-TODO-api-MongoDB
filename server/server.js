dc7f50c7c8df9d0cdd7b9f213de6a3a8ed6557da
marinaysn <marinaysn@gmail.com>
2018-12-22 13:58:20 -0500

    add GET /todos

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 //library imports
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 //local imports
-const { mongoose } = require('./db/mongoose.js');
-const { Todo } = require('./models/todo');
-const { Users } = require('./models/users');
+const { mongoose } = require("./db/mongoose.js");
+const { Todo } = require("./models/todo");
+const { Users } = require("./models/users");
 
 const app = express();
 app.use(bodyParser.json()); // middlewere
 
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
   //console.log(req.body);
 
   let todo = new Todo({
@@ -19,17 +19,32 @@ app.post('/todos', (req, res) => {
     completedAt: req.body.completedAt
   });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
       res.status(400).send(e);
-  });
+    }
+  );
 });
 
-
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    (todos) => {
+      res.send({
+        todos,
+        code: 123
+      });
+    },
+    e => {
+      res.status(404).send(e)
+    }
+  );
+});
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
-})
+  console.log("Started on port 3000");
+});
 
 module.exports = { app };
