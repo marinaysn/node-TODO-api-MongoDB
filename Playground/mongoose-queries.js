06684feb1094f673d8e5fff34933988f106819b3
marinaysn <marinaysn@gmail.com>
2018-12-27 16:32:49 -0500

    add quesries to PLG

---
+++ Playground/mongoose-queries.js
@@ -0,0 +1,11 @@
+//const { mongoose } = require("./../server/db/mongoose");
+//const { Todo } = require("/../server/models/todo");
+
+import { mongoose } from "./../server/db/mongoose";
+import { Todo } from "/../server/models/todo";
+
+
+
+
+
+
