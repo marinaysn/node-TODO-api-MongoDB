633782a8b4b98b0fe70d9c3606a43611b6cdacbd
marinaysn <marinaysn@gmail.com>
2018-12-27 18:04:59 -0500

    add/tested findByIdAndDelete

--- Playground/mongoose-queries.js
+++ Playground/mongoose-queries.js
@@ -6,12 +6,13 @@ const { Todo } = require("./../server/models/todo");
 
 
 let id = '5c254c076a26881e743c3fef';
+let id2 = '5c1c7983cbac463ee81312d0';
 
 Todo.find({
     _id: id,
     completed: false
 }).then((todos) => {
-    console.log('Todos', todos)
+    console.log('All Todos', todos)
 });
 
 Todo.findOne({
@@ -22,6 +23,14 @@ Todo.findOne({
 
 
 Todo.findById(id).then((todo) => {
-    console.log('Todo: ', todo)
+    console.log('Todo by ID: ', todo)
+});
+
+// Todo.findByIdAndDelete(id2).then((todo) => {
+//     console.log('Todo by ID: ', todo)
+// });
+
+Todo.findById(id2).then((todo) => {
+    console.log('Todo by ID: ', todo)
 });
 
