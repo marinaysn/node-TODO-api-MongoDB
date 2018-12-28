19fe0d1902316823b94a3fd7be3f0af3e2f99110
marinaysn <marinaysn@gmail.com>
2018-12-27 23:26:54 -0500

    add/tested findOneAndUpdate

--- Playground/mongoose-queries.js
+++ Playground/mongoose-queries.js
@@ -6,31 +6,35 @@ const { Todo } = require("./../server/models/todo");
 
 
 let id = '5c254c076a26881e743c3fef';
-let id2 = '5c1c7983cbac463ee81312d0';
+let id2 = '5c1b3969e07cc13f10b66152';
 
-Todo.find({
-    _id: id,
-    completed: false
-}).then((todos) => {
-    console.log('All Todos', todos)
-});
+// Todo.find({
+//     _id: id,
+//     completed: false
+// }).then((todos) => {
+//     console.log('All Todos', todos)
+// });
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo: ', todo)
-});
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo: ', todo)
+// });
 
 
-Todo.findById(id).then((todo) => {
-    console.log('Todo by ID: ', todo)
-});
+// Todo.findById(id).then((todo) => {
+//     console.log('Todo by ID: ', todo)
+// });
 
 // Todo.findByIdAndDelete(id2).then((todo) => {
 //     console.log('Todo by ID: ', todo)
 // });
 
-Todo.findById(id2).then((todo) => {
-    console.log('Todo by ID: ', todo)
-});
+// Todo.findById(id2).then((todo) => {
+//     console.log('Todo by ID: ', todo)
+// });
+
+Todo.findOneAndUpdate(id2, { $set: {text: "Call Jane / New York", completed: true}}).then((todo) => {
+    console.log('findOneAndUpdate: ', todo)
+}); 
 
