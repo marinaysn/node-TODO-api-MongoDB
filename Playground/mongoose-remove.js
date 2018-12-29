52f4cbb79d4dd05d458e2be3eb7a6b60bc6f9503
marinaysn <marinaysn@gmail.com>
2018-12-28 23:49:29 -0500

    add deleted queries file

---
+++ Playground/mongoose-remove.js
@@ -0,0 +1,6 @@
+const { ObjectId } = require("mongodb");
+
+const { mongoose } = require("./../server/db/mongoose");
+const { Todo } = require("./../server/models/todo");
+const { User } = require("./../server/models/users");
+
