fbb3e01f68ea679f9e587f18b3f8c544d7987ad1
marinaysn <marinaysn@gmail.com>
2018-12-28 19:50:41 -0500

    Setup App for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,9 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://127.0.0.1:27017/TodoMApp',
-  mlab: 'mongodb://marinaysn:enferced88mongodb://<dbuser>:<dbpassword>@ds145304.mlab.com:45304/todoapp'
+  mlab: 'mongodb://<dbuser>:<dbpassword>@ds145304.mlab.com:45304/todoapp'
 };
-mongoose.connect(process.env.MONGODB_URI ? db.mlab : db.localhost, { useNewUrlParser: true })
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost, { useNewUrlParser: true })
 
 // mongoose.connect( process.env.MONGODB_URI ||
 //   "mongodb://127.0.0.1:27017/TodoMApp",
