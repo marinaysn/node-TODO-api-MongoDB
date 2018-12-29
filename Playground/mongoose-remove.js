ff2d7867034c54df3ebbb9bd14ec532ceba3bcc5
marinaysn <marinaysn@gmail.com>
2018-12-29 00:04:15 -0500

    updated the remove queries with findOneAndDelete

--- Playground/mongoose-remove.js
+++ Playground/mongoose-remove.js
@@ -4,3 +4,22 @@ const { mongoose } = require("./../server/db/mongoose");
 const { Todo } = require("./../server/models/todo");
 const { User } = require("./../server/models/users");
 
+// will remove everything from the list
+// Todo.remove({}).then((result) => {
+//     console.log("Everythign is removed");
+//     console.log(result);
+//     
+// })
+
+//other remove methods
+//Todo.findOneAndRemove({})
+//Todo.findByIdAndRemove()
+//Todo.findOneAndDelete({})
+//Todo.findByIdAndDelete()
+
+let id = '5c1e9df56984de2a2cba4fdd'; //"text" : "Wash dishes and do tables"
+
+Todo.findByIdAndDelete(id).then((todo) => {
+    console.log(todo);
+})
+
