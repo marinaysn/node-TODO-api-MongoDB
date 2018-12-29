4132ed41bcd01af6dccc339b7728c05b93ab4f75
marinaysn <marinaysn@gmail.com>
2018-12-28 20:54:11 -0500

    fix error with connections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://127.0.0.1:27017/TodoMApp',
-  mlab: 'mongodb://<dbuser>:<dbpassword>@ds145304.mlab.com:45304/todoapp'
-};
-mongoose.connect(process.env.POST ? db.mlab : db.localhost, { useNewUrlParser: true })
+// let db = {
+//   localhost: 'mongodb://127.0.0.1:27017/TodoMApp',
+//   mlab: 'mongodb://<dbuser>:<dbpassword>@ds145304.mlab.com:45304/todoapp'
+// };
+// mongoose.connect(process.env.POST ? db.mlab : db.localhost, { useNewUrlParser: true })
 
-// mongoose.connect( process.env.MONGODB_URI ||
-//   "mongodb://127.0.0.1:27017/TodoMApp",
-//   { useNewUrlParser: true }
-// );
+mongoose.connect( process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1:27017/TodoMApp",
+  { useNewUrlParser: true }
+);
 
 module.exports = {
     mongoose
