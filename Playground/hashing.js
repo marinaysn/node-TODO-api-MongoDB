5e7e8d380a461824d7740cd901cb78c6cb362730
marinaysn <marinaysn@gmail.com>
2019-01-02 23:23:54 -0500

    added hashing ot playground for learning/testing purposes

---
+++ Playground/hashing.js
@@ -0,0 +1,30 @@
+const {SHA256} = require('crypto-js');
+
+// let msg = 'I am user number 3';
+// let hash = SHA256(msg).toString();
+// console.log(`${msg} \n\n${hash}secret\n\n`);
+
+
+// let data = {
+    
+//     id : 4
+// };
+
+// let token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+    
+// data.id = 5;
+// let = resultHash = SHA256(JSON.stringify(data) + "somesecret").toString();
+
+
+
+// if (resultHash === token.hash) {
+//     console.log("Data was not changed")
+// }
+// else {
+//     console.log("Data was changed")
+// }
+
+
