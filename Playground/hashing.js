2b3b226bf68c9c156b2065911db7ee674c1a5ce4
marinaysn <marinaysn@gmail.com>
2019-01-03 00:02:09 -0500

    changes to hashing /jsonwebtoken

--- Playground/hashing.js
+++ Playground/hashing.js
@@ -1,4 +1,25 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken')
+
+
+
+let data = {
+    
+    id: 10,
+    name: 'marka'
+};
+
+let token = jwt.sign(data, 'somesecretcode');
+
+
+console.log(`${token}\n\n`);
+
+
+let decodedResults = jwt.verify(token, 'somesecretcode')
+
+console.log('decodedResults', decodedResults);
+
+//data.name='marina'
 
 // let msg = 'I am user number 3';
 // let hash = SHA256(msg).toString();
